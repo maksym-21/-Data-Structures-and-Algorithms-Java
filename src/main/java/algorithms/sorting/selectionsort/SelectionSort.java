756cb3d6d9ca71bbc49ab432479b30aa0b5ac96b
mbuniak <mbuniak@griddynamics.com>
2022-10-08 13:59:07 +0200

    ada

--- src/main/java/algorithms/sorting/selectionsort/SelectionSort.java
+++ src/main/java/algorithms/sorting/selectionsort/SelectionSort.java
@@ -8,12 +8,14 @@ public class SelectionSort implements SortingAlgorithm {
             int tmpMinValue = array[i];
             int tmpValue = tmpMinValue;
             int minIndex = i;
+
             for (int j = i + 1; j < array.length; j++) {
                 if (array[j] < tmpValue) {
                     minIndex = j;
                     tmpValue = array[j];
                 }
             }
+
             array[i] = array[minIndex];
             array[minIndex] = tmpMinValue;
         }
