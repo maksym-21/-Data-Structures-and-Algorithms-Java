9d672db7986b290fac862a30c7c7fd203521d24a
mbuniak <mbuniak@griddynamics.com>
2022-10-13 15:49:41 +0200

    bla bla

--- src/main/java/algorithms/sorting/selectionsort/SelectionSort.java
+++ src/main/java/algorithms/sorting/selectionsort/SelectionSort.java
@@ -5,10 +5,11 @@ import algorithms.sorting.SortingAlgorithm;
 public class SelectionSort implements SortingAlgorithm {
     public void sort(int[] array) {
         for (int i = 0; i < array.length; i++) {
+
+
             int tmpMinValue = array[i];
             int tmpValue = tmpMinValue;
             int minIndex = i;
-
             for (int j = i + 1; j < array.length; j++) {
                 if (array[j] < tmpValue) {
                     minIndex = j;
